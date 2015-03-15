79b4955f3d032c1bf47d3f8ae512b492fa61b981
mvanegas10 <m.vanegas10@uniandes.edu.co>
2015-03-15 18:58:03 -0500

    asd

--- src/Interfaz/ServletRegistrarMateriaPrima.java
+++ src/Interfaz/ServletRegistrarMateriaPrima.java
@@ -34,7 +34,44 @@ public class ServletRegistrarMateriaPrima extends ServletAbstract {
 	}
 	
 	public void hayMateriasPrima (PrintWriter respuesta, ArrayList<MateriaPrima> materias){
-		
+		respuesta.write( "<style>" );
+		respuesta.write( "SELECT, INPUT[type=\"text\"] {" );
+		respuesta.write( "width: 160px;" );
+		respuesta.write( "box-sizing: border-box;" );
+		respuesta.write( "}" );
+		respuesta.write( "SECTION {" );
+		respuesta.write( "padding: 8px;" );
+		respuesta.write( "background-color: #f0f0f0;" );
+		respuesta.write( "overflow: auto;" );
+		respuesta.write( "}" );
+		respuesta.write( "SECTION > DIV {" );
+		respuesta.write( "float: left;" );
+		respuesta.write( "padding: 4px;" );
+		respuesta.write( "}" );
+		respuesta.write( "SECTION > DIV + DIV {" );
+		respuesta.write( "width: 40px;" );
+		respuesta.write( "text-align: center;" );
+		respuesta.write( "}</style>" );
+		respuesta.write( "<section class=\"container\">" );
+		respuesta.write( " <div>" );
+		respuesta.write( " <select id=\"leftValues\" size=\"5\" multiple></select>" );
+		respuesta.write( " </div>" );
+		respuesta.write( " <div>" );
+		respuesta.write( " <input type=\"button\" id=\"btnLeft\" value=\"&lt;&lt;\" />" );
+		respuesta.write( " <input type=\"button\" id=\"btnRight\" value=\"&gt;&gt;\" />" );
+		respuesta.write( " </div>" );
+		respuesta.write( " <div>" );
+		respuesta.write( " <select id=\"rightValues\" size=\"4\" multiple>" );
+		for (MateriaPrima materiaPrima : materias) {
+			respuesta.write( " <option>" + materiaPrima.getId() + "</option>" );	
+		}
+		respuesta.write( " <option>1</option>" );
+		respuesta.write( " <option>2</option>" );
+		respuesta.write( " <option>3</option>" );
+		respuesta.write( " </select>" );
+		respuesta.write( " <div>" );
+		respuesta.write( " <input type=\"text\" id=\"txtRight\" />" );
+		respuesta.write( " </div>" );
 	}
 	
 	public void noHayMateriasPrima (PrintWriter respuesta){
