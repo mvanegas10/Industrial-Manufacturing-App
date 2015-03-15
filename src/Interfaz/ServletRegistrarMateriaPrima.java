16d20595932107a6443121d594d6dc9623299785
mvanegas10 <m.vanegas10@uniandes.edu.co>
2015-03-15 18:32:54 -0500

    Holeee

--- src/Interfaz/ServletRegistrarMateriaPrima.java
+++ src/Interfaz/ServletRegistrarMateriaPrima.java
@@ -2,11 +2,13 @@ package Interfaz;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import mundo.AplicacionWeb;
+import mundo.MateriaPrima;
 
 public class ServletRegistrarMateriaPrima extends ServletAbstract {
 
@@ -19,7 +21,28 @@ public class ServletRegistrarMateriaPrima extends ServletAbstract {
 	public void escribirContenido(HttpServletRequest request,HttpServletResponse response) throws IOException {
 
 		PrintWriter respuesta = response.getWriter( );
+		try
+		{
+			ArrayList<MateriaPrima> materiasPrima = AplicacionWeb.darInstancia().darMateriasPrimas();
+			hayMateriasPrima(respuesta, materiasPrima);
+		}
+		catch(Exception e)
+		{
+			noHayMateriasPrima (respuesta);
+		}
 
+	}
+	
+	public void hayMateriasPrima (PrintWriter respuesta, ArrayList<MateriaPrima> materias){
+		
+	}
+	
+	public void noHayMateriasPrima (PrintWriter respuesta){
+        respuesta.write( "<table bgcolor=\"#ecf0f1\" width=80%>" );
+        respuesta.write( "<tr>" );
+        respuesta.write( "<td><FONT face=\"arial\" size=5 color=#34495e>Error: No hay registrada ninguna materia prima, primero registre materias prima a ProdAndes</FONT></td>" );
+        respuesta.write( "</tr>" );
+        respuesta.write( "</table>" );
 	}
 
 }
